448b6e280ac7b11885906b61577fb7ce4ce907f6
Brady Brown <brbrown@hubspot.com>
2022-04-27 23:13:18 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,19 @@
+## minimal-nnn.nvim
+
+The most minimal nnn plugin for neovim ever
+
+![demo gif](demo.gif)
+
+### Install
+```lua
+
+    -- in your packer config
+    use({"bobrown101/minimal-nnn.nvim"})
+
+    -- tied to a keymap of your choice
+    require("minimal-nnn").start()
+
+```
+
+## Description
+Made possible by nvim --remote, we start up an nnn picker in a standard neovim terminal, and "callback" the result to the current neovim instance.
---
+++ lua/minimal-nnn.lua
@@ -0,0 +1,33 @@
+local M = {}
+
+function M.start()
+
+    -- start a socket that we can listen for the result on
+    local socketname = vim.fn.tempname()
+    vim.fn.serverstart(socketname)
+
+    -- generate the nnn command with the right parameters
+    local currentLocation = vim.fn.expand('%:p:h')
+    local filepickercommand = 'nnn -p - ' .. currentLocation
+
+    -- use nvim --server to "callback" over rpc the result of "filepickercommand"
+    local callbackcommand = string.format([[nvim --server %s --remote $(%s) ]],
+                                          socketname, filepickercommand)
+
+    -- open up the callbackcommand (which already contains the filepickercommand) in a new terminal buffer
+    vim.cmd('term ' .. callbackcommand)
+    -- when entering the terminal, automatically enter in insert mode
+    vim.cmd('startinsert')
+
+    -- lets grab the buffer number of the terminal buffer we just made
+    local termbuffnumber = vim.api.nvim_get_current_buf()
+
+    -- upon closing the terminal (aka, when nnn and the callback process exits), delete the buffer
+    vim.api.nvim_create_autocmd({"TermClose"}, {
+        buffer = termbuffnumber,
+        command = "bd " .. termbuffnumber
+    })
+
+end
+
+return M
